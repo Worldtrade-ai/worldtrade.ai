1910b9a29ddb088a166d4204e07208728a549b68
LEVIANA <princejoaquineleonardo@gmail.com>
2026-04-12 12:43:36 +0800

    update

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
 // Import Firebase modules
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  onAuthStateChanged
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";    
 
 // Firebase configuration
 const firebaseConfig = {
