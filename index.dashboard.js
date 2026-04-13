aff391f43ece18785968081eaa351c702bbc096c
LEVIANA <princejoaquineleonardo@gmail.com>
2026-04-13 12:39:25 +0800

    Update and rename index.dashboard.js to dashboard.html
    1

--- index.dashboard.js
+++
@@ -1,9 +0,0 @@
-// Check if user is logged in
-firebase.auth().onAuthStateChanged((user) => {
-  if (user) {
-    document.getElementById("userEmail").innerText = user.email;
-  } else {
-    // Not logged in → go back to login page
-    window.location.href = "login.html";
-  }
-});
